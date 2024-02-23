40cb226434e004aec6678ed9e24662111224705a
Andrés Calle <andrescalleuribe@gmail.com>
2024-02-23 14:06:48 -0500

    Mongoose implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/tasksdb'),
+    TaskModule
+  ],
 })
 export class AppModule {}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import {Task} from '../scheme/task.schema'
+import {Model} from 'mongoose'
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+    constructor(@InjectModel(Task.name) private taskModel:Model<Task>){}
+
+    async findAll(){
+        return await this.taskModel.find()
+    }
+    async create(data:{title:string,description?:string}){
+        return await this.taskModel.create(data)
+    }
+    async update(id:string,data:{title?:string,description?:string,done?:boolean}){
+        return await this.taskModel.findByIdAndUpdate
+        (id,data,{new:true})
+    }
+    async findOne(id:string){
+        return await this.taskModel.findById(id)
+    }
+    async delete(id:string){
+        return await this.taskModel.findByIdAndDelete(id)
+    }
+}
