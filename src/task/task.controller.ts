587d43d78886ac158bc43c7a2dbf5f2bff5feda6
Andrés Calle <andrescalleuribe@gmail.com>
2024-02-23 15:33:26 -0500

    Endpoints & validation working

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { TaskService } from './task.service';
+import {CreateTaskDto } from '../dto/create-task.dto'
 
-@Controller('task')
-export class TaskController {}
+@Controller('tasks')
+export class TaskController {
+    constructor(private taskService:TaskService){}
+    @Get()
+    async findAll(){
+        return await this.taskService.findAll()
+    }
+    @Get(':id')
+    async findOne(id:string){
+        return await this.taskService.findOne(id)
+    }
+    @Post()
+    async create(@Body() body:CreateTaskDto){
+        return await this.taskService.create(body)
+    }
+    @Put(':id')
+    async update(id:string,data){
+        return await this.taskService.update(id,data)
+    }
+    @Delete(':id')
+    async delete(id:string){
+        return await this.taskService.delete(id)
+    }
+}
